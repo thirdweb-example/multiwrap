e817d3156ed4091845b7f1186b55e10aa352a57e
Jarrod Watts <jarrodwatts16@gmail.com>
2022-06-03 17:07:43 +1000

    Check if there are tokens before adding them into the wrap

--- components/CustomContract.jsx
+++ components/CustomContract.jsx
@@ -1,12 +1,13 @@
 import { useContract } from "@thirdweb-dev/react";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export const CustomContract = ({ setContract, contractAddress }) => {
+  // Fetch the contract from the blockchain by its address
   const { contract } = useContract(contractAddress);
 
+  // Whenever the contract is passed in, fetch the contract and set it in the state
   useEffect(() => {
-    setContract(state => ({ ...state, contract }));
-    console.log(contract);
+    setContract((state) => ({ ...state, contract }));
   }, [contract, setContract, contractAddress]);
 
   return null;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-import { useAddress, useDisconnect, useMetamask, useMultiwrap } from '@thirdweb-dev/react';
-import { CustomContract } from '../components/CustomContract';
-import { useState } from 'react';
-import {BigNumber, ethers} from "ethers";
+import {
+  useAddress,
+  useDisconnect,
+  useMetamask,
+  useMultiwrap,
+  useToken,
+} from "@thirdweb-dev/react";
+import { CustomContract } from "../components/CustomContract";
+import { useState } from "react";
+import { ethers } from "ethers";
 
 const multiwrapAddress = "0xc84A8236e5dE1F1DD67aa6156A93155A9B0B4f6a";
 
@@ -11,61 +17,96 @@ export default function Home() {
   const disconnectWallet = useDisconnect();
   const multiwrap = useMultiwrap(multiwrapAddress);
 
-  const [erc721, setErc721] = useState({ contract: null, tokenId: null, address: null });
-  const [erc1155, setErc1155] = useState({ contract: null, tokenId: null, address: null, quantity: null });
-  const [erc20, setErc20] = useState({ contract: null, address: null, quantity: null });
+  // Store the state of each input fields
+  const [erc721, setErc721] = useState({
+    contract: null,
+    tokenId: null,
+    address: null,
+  });
+  const [erc1155, setErc1155] = useState({
+    contract: null,
+    tokenId: null,
+    address: null,
+    quantity: null,
+  });
+  const [erc20, setErc20] = useState({
+    contract: null,
+    address: null,
+    quantity: null,
+  });
 
   const wrap = async (e) => {
+    // If there is a value for each erc721, erc1155, and erc20, include them in array
+
     e.preventDefault();
     const tokensToWrap = {
-      erc20Tokens: [
-        {
-          contractAddress: erc20.address,
-          quantity: erc20.quantity,
-        },
-      ],
-      erc721Tokens: [
-        {
-          contractAddress: erc721.address,
-          tokenId: erc721.tokenId,
-        },
-      ],
-      erc1155Tokens: [
-        {
-          contractAddress: erc1155.address,
-          tokenId: erc1155.tokenId,
-          quantity: erc1155.quantity,
-        },
-      ],
+      erc20Tokens: erc20.contract
+        ? [
+            {
+              contractAddress: erc20.address,
+              quantity: erc20.quantity,
+            },
+          ]
+        : [],
+      erc721Tokens: erc721.contract
+        ? [
+            {
+              contractAddress: erc721.address,
+              tokenId: erc721.tokenId,
+            },
+          ]
+        : [],
+      erc1155Tokens: erc1155.contract
+        ? [
+            {
+              contractAddress: erc1155.address,
+              tokenId: erc1155.tokenId,
+              quantity: erc1155.quantity,
+            },
+          ]
+        : [],
     };
 
     try {
-      if (erc20.contract !== null ) {
-        //@TODO Get contract decimals dynamically
-        const balance = await erc20.contract.call("balanceOf", address);
-        console.log(ethers.utils.parseUnits(erc20.quantity, 18));
-        const perm = await erc20.contract.call("approve", multiwrapAddress, ethers.utils.parseUnits(erc20.quantity, 18));
-        console.log("Permission Request", perm);
+      // Approve each of the tokens for transfer into the multiwrap NFT
+
+      // 1. ERC 20 tokens
+      if (erc20.contract !== null) {
+        const approveErc20Transfer = await erc20.contract.call(
+          "approve",
+          multiwrapAddress,
+          // This is formatting the quantity to 18 decimal places
+          ethers.utils.parseUnits(erc20.quantity, 18)
+        );
       }
+
+      // 2. ERC1155 tokens
       if (erc1155.contract !== null) {
-        await erc1155.contract.call("approve", multiwrapAddress, erc1155.tokenId);
+        await erc1155.contract.call(
+          "approve",
+          multiwrapAddress,
+          erc1155.tokenId
+        );
       }
+
+      // 3. ERC721 tokens
       if (erc721.contract !== null) {
-        console.log("It's trying stupid")
         await erc721.contract.call("approve", multiwrapAddress, erc721.tokenId);
       }
 
-      await multiwrap.wrap(tokensToWrap, { name: "My Wrapped Token" });
+      // Now we have the approval, we can wrap them all together
+      const newToken = await multiwrap.wrap(tokensToWrap, {
+        name: "My Wrapped Token",
+      });
+
+      alert("Successfully wrapped!");
     } catch (error) {
       console.log(error);
     }
   };
 
-
   if (!address) {
-    return (
-      <button onClick={connectWithMetamask}>Connect with Metamask</button>
-    );
+    return <button onClick={connectWithMetamask}>Connect with Metamask</button>;
   }
 
   return (
@@ -75,22 +116,75 @@ export default function Home() {
 
       <form>
         <div>
-          <CustomContract setContract={setErc721} contractAddress={erc721.address} />
-          <input type="text" placeholder="ERC721 contract address" onChange={(e) => setErc721({ ...erc721, address: e.target.value })} />
-          <input type="text" placeholder="ERC721 token ID" onChange={(e) => setErc721({ ...erc721, tokenId: e.target.value })} />
+          <CustomContract
+            setContract={setErc721}
+            contractAddress={erc721.address}
+          />
+          <input
+            type="text"
+            placeholder="ERC721 contract address"
+            onChange={(e) => setErc721({ ...erc721, address: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="ERC721 token ID"
+            onChange={(e) => setErc721({ ...erc721, tokenId: e.target.value })}
+          />
         </div>
+
         <div>
-          <CustomContract setContract={setErc1155} contractAddress={erc1155.address} />
-          <input type="text" placeholder="ERC1155 contract address" onChange={(e) => setErc1155({ ...erc1155, address: e.target.value })} />
-          <input type="text" placeholder="ERC1155 token ID" onChange={(e) => setErc1155({ ...erc1155, tokenId: e.target.value })} />
-          <input type="text" placeholder="ERC1155 quantity" onChange={(e) => setErc1155({ ...erc1155, quantity: e.target.value })} />
+          <CustomContract
+            setContract={setErc1155}
+            contractAddress={erc1155.address}
+          />
+          <input
+            type="text"
+            placeholder="ERC1155 contract address"
+            onChange={(e) =>
+              setErc1155({ ...erc1155, address: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="ERC1155 token ID"
+            onChange={(e) =>
+              setErc1155({ ...erc1155, tokenId: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="ERC1155 quantity"
+            onChange={(e) =>
+              setErc1155({ ...erc1155, quantity: e.target.value })
+            }
+          />
         </div>
+
         <div>
-          <CustomContract setContract={setErc20} contractAddress={erc20.address} />
-          <input type="text" placeholder="ERC20 contract address" onChange={(e) => setErc20({ ...erc20, address: e.target.value })} />
-          <input type="text" placeholder="ERC20 quantity" onChange={(e) => setErc20({ ...erc20, quantity: e.target.value })} />
+          <CustomContract
+            setContract={setErc20}
+            contractAddress={erc20.address}
+            useFunction={useToken}
+          />
+          <input
+            type="text"
+            placeholder="ERC20 contract address"
+            onChange={(e) => setErc20({ ...erc20, address: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="ERC20 quantity"
+            onChange={(e) => setErc20({ ...erc20, quantity: e.target.value })}
+          />
         </div>
-        <button type="submit" onClick={wrap}>Wrap</button>
+
+        <button type="submit" onClick={wrap}>
+          Wrap
+        </button>
+
+        <hr />
+
+        {/* TODO Get all */}
       </form>
     </div>
   );
